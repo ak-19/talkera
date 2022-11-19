b24086ff2f4702f8faedfefb565413475c7cdde4
ante <ante.kotarac@gmail.com>
2022-11-19 15:46:28 +0100

    changes to intial template

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "This is root"', () => {
+      expect(appController.getHello()).toBe('This is root');
     });
   });
 });
